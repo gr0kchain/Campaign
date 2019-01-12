a9b582a24ab9dbd020b483c0b2dbcbcdb643b757
Vidit Shah <vidit0210@gmail.com>
2019-01-12 15:29:51 +0530

    Address

--- Ethereum/deploy.js
+++ Ethereum/deploy.js
@@ -1,6 +1,6 @@
 const HDWalletrovider = require("truffle-hdwallet-provider");
 const Web3 = require("web3");
-const { interface, bytecode } = require("./compile");
+const compiledFactory = require("./build/FactoryCampaign.json");
 //Hd wallet - 2 arguments -mnemonic(12 word phrase),infura link for test net
 
 const provider = new HDWalletrovider(
@@ -11,12 +11,13 @@ const web3 = new Web3(provider);
 async function deploy() {
   const accounts = await web3.eth.getAccounts();
   console.log("Attempting to Deploy from Account:", accounts[0]);
-  const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode })
+  const result = await new web3.eth.Contract(
+    JSON.parse(compiledFactory.interface)
+  )
+    .deploy({ data: compiledFactory.bytecode })
     .send({ from: accounts[0], gas: 1000000 });
   //IMPORTANT STEP where contract got deployed
   //We Will Add this to our React Networka
-  console.log(interface);
   console.log("Contract deployed to:", result.options.address);
 }
 deploy();
